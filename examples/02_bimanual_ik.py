783194ee6382a4fcfd2c0acabdbd8bf2b1ad0338
ariszxxu <ariszxxu@gmail.com>
2026-03-09 17:52:43 +0800

    init

--- examples/02_bimanual_ik.py
+++ examples/02_bimanual_ik.py
@@ -28,17 +28,17 @@ def main():
 
     # openarm_bimanual
     urdf = yourdfpy.URDF.load(
-        "/home/ps/ros2_ws/openarm_bimanual.urdf",
-        mesh_dir="/home/ps/ros2_ws"
+        "../thirdparty/ros2_ws_1/openarm_bimanual.urdf",
+        mesh_dir="../thirdparty/ros2_ws_1"
     )
     target_link_names = ["openarm_right_hand", "openarm_left_hand"] # 需要移动urdf到阳间位置
 
     # g1_dual_arm
-    urdf = yourdfpy.URDF.load(
-        "../thirdparty/unitree_rl_gym/resources/robots/g1_description/g1_dual_arm.urdf",
-        mesh_dir="../thirdparty/unitree_rl_gym/resources/robots/g1_description"
-    )
-    target_link_names = ["right_rubber_hand", "left_rubber_hand"]
+    # urdf = yourdfpy.URDF.load(
+    #     "../thirdparty/unitree_rl_gym/resources/robots/g1_description/g1_dual_arm.urdf",
+    #     mesh_dir="../thirdparty/unitree_rl_gym/resources/robots/g1_description"
+    # )
+    # target_link_names = ["right_rubber_hand", "left_rubber_hand"]
     
     # Create robot.
     robot = pk.Robot.from_urdf(urdf)
@@ -52,24 +52,29 @@ def main():
     robot_base = server.scene.add_frame("/robot")
     urdf_vis = ViserUrdf(server, urdf, root_node_name="/robot")
 
-    base_offset = np.array([0, 0, -0.1])
+    base_offset = np.array([0, 0, 0.5])
     robot_base.position = base_offset
 
     # Create interactive controller with initial position.
-    # for g1_dual_arm ik_target.wxyz = (1,0,0,0) is good, for openarm_bimanual ik_target.wxyz = (0,0,1,0) is good.
     ik_target_0 = server.scene.add_transform_controls(
-        "/ik_target_0", scale=0.2, position=(0.41, -0.3, 0.56), wxyz=(1,0,0,0)
+        "/ik_target_0", scale=0.2, position=(0.41, -0.3, 0.56), wxyz=(0,0,1,0)
     )
     ik_target_1 = server.scene.add_transform_controls(
-        "/ik_target_1", scale=0.2, position=(0.41, 0.3, 0.56), wxyz=(1,0,0,0)
+        "/ik_target_1", scale=0.2, position=(0.41, 0.3, 0.56), wxyz=(0,0,1,0)
     )
     timing_handle = server.gui.add_number("Elapsed (ms)", 0.001, disabled=True)
 
-    target0_pos = server.gui.add_text("target0 position", initial_value=str(np.round(ik_target_0.position - base_offset, 3)))
-    target0_rot = server.gui.add_text("target0 quaternion", initial_value=str(np.round(ik_target_0.wxyz, 3)))
+    target0_pos_robot = server.gui.add_text("target0 pos robot", initial_value=str(np.round(ik_target_0.position - base_offset, 3)))
+    target0_rot_robot = server.gui.add_text("target0 quat robot", initial_value=str(np.round(ik_target_0.wxyz, 3)))
 
-    target1_pos = server.gui.add_text("target1 position", initial_value=str(np.round(ik_target_1.position - base_offset, 3)))
-    target1_rot = server.gui.add_text("target1 quaternion", initial_value=str(np.round(ik_target_1.wxyz, 3)))
+    target1_pos_robot = server.gui.add_text("target1 pos robot", initial_value=str(np.round(ik_target_1.position - base_offset, 3)))
+    target1_rot_robot = server.gui.add_text("target1 quat robot", initial_value=str(np.round(ik_target_1.wxyz, 3)))
+
+    target0_pos_world = server.gui.add_text("target0 pos world", initial_value=str(np.round(ik_target_0.position, 3)))
+    target0_rot_world = server.gui.add_text("target0 quat world", initial_value=str(np.round(ik_target_0.wxyz, 3)))
+
+    target1_pos_world = server.gui.add_text("target1 pos world", initial_value=str(np.round(ik_target_1.position, 3)))
+    target1_rot_world = server.gui.add_text("target1 quat world", initial_value=str(np.round(ik_target_1.wxyz, 3)))
 
     while True:
         # Solve IK.
@@ -86,11 +91,17 @@ def main():
         elapsed_time = time.time() - start_time
         timing_handle.value = 0.99 * timing_handle.value + 0.01 * (elapsed_time * 1000)
 
-        target0_pos.value = str(np.round(ik_target_0.position, 3))
-        target0_rot.value = str(np.round(ik_target_0.wxyz, 3))
+        target0_pos_robot.value = str(np.round(ik_target_0.position - base_offset, 3))
+        target0_rot_robot.value = str(np.round(ik_target_0.wxyz, 3))
 
-        target1_pos.value = str(np.round(ik_target_1.position, 3))
-        target1_rot.value = str(np.round(ik_target_1.wxyz, 3))
+        target1_pos_robot.value = str(np.round(ik_target_1.position - base_offset, 3))
+        target1_rot_robot.value = str(np.round(ik_target_1.wxyz, 3))
+
+        target0_pos_world.value = str(np.round(ik_target_0.position, 3))
+        target0_rot_world.value = str(np.round(ik_target_0.wxyz, 3))
+
+        target1_pos_world.value = str(np.round(ik_target_1.position, 3))
+        target1_rot_world.value = str(np.round(ik_target_1.wxyz, 3))
 
         # Update visualizer.
         urdf_vis.update_cfg(solution)
--- examples/02_multi_bimanual_ik.py
+++ examples/02_multi_bimanual_ik.py
@@ -1,87 +1,150 @@
 import time
 import viser
 import yourdfpy
-from robot_descriptions.loaders.yourdfpy import load_robot_description
 import numpy as np
 import pyroki as pk
 from viser.extras import ViserUrdf
 import pyroki_snippets as pks
+
 
 def load_robot(path, mesh_dir):
     urdf = yourdfpy.URDF.load(path, mesh_dir=mesh_dir)
     robot = pk.Robot.from_urdf(urdf)
     return urdf, robot
 
+
 def main():
-    # Set up visualizer.
+
     server = viser.ViserServer()
     server.scene.add_grid("/ground", width=2, height=2)
 
-    # dual_xarm7
+    # ------------------------------------------------
+    # load robots
+    # ------------------------------------------------
+
     urdf1, robot1 = load_robot(
-        path = "../assert/xarm7/dual_xarm7.urdf",
-        mesh_dir="../assert/xarm7"
+        "../assert/xarm7/dual_xarm7.urdf",
+        "../assert/xarm7"
     )
     target_link_names_1 = ["right_link_eef", "left_link_eef"]
 
-    # openarm_bimanual
     urdf2, robot2 = load_robot(
-        path = "/home/ps/ros2_ws/openarm_bimanual.urdf",
-        mesh_dir="/home/ps/ros2_ws"
+        "../thirdparty/ros2_ws_1/openarm_bimanual.urdf",
+        "../thirdparty/ros2_ws_1"
     )
-    target_link_names_2 = ["openarm_right_hand", "openarm_left_hand"] # 需要移动urdf到阳间位置
+    target_link_names_2 = ["openarm_right_hand", "openarm_left_hand"]
 
-    # g1_dual_arm
     urdf3, robot3 = load_robot(
-        path = "../thirdparty/unitree_rl_gym/resources/robots/g1_description/g1_dual_arm.urdf",
-        mesh_dir="../thirdparty/unitree_rl_gym/resources/robots/g1_description"
+        "../thirdparty/unitree_rl_gym/resources/robots/g1_description/g1_dual_arm.urdf",
+        "../thirdparty/unitree_rl_gym/resources/robots/g1_description"
     )
     target_link_names_3 = ["right_rubber_hand", "left_rubber_hand"]
 
-    vis1 = ViserUrdf(server, urdf1, root_node_name="/dual_xarm7")
-    vis2 = ViserUrdf(server, urdf2, root_node_name="/openarm_bimanual")
-    vis3 = ViserUrdf(server, urdf3, root_node_name="/g1_dual_arm")
+    # ------------------------------------------------
+    # create robot base frames
+    # ------------------------------------------------
 
-    # Create interactive controller with initial position.
+    base1 = server.scene.add_frame("/robot1")
+    base2 = server.scene.add_frame("/robot2")
+    base3 = server.scene.add_frame("/robot3")
+
+    offset1 = np.array([0.0, 0.0, -0.3]) # dual_xarm7
+    offset2 = np.array([0.0, 0.0, -0.3]) # openarm_bimanual
+    offset3 = np.array([0.0, 0.0, -0.1]) # g1_dual_arm
+
+    base1.position = offset1
+    base2.position = offset2
+    base3.position = offset3
+
+    # ------------------------------------------------
+    # attach URDF to frames
+    # ------------------------------------------------
+
+    vis1 = ViserUrdf(server, urdf1, root_node_name="/robot1")
+    vis2 = ViserUrdf(server, urdf2, root_node_name="/robot2")
+    vis3 = ViserUrdf(server, urdf3, root_node_name="/robot3")
+
+    # ------------------------------------------------
+    # IK targets
+    # ------------------------------------------------
+
     ik_target_0 = server.scene.add_transform_controls(
-        "/ik_target_0", scale=0.2, position=(0.41, -0.3, 0.56), wxyz=(1, 0, 0, 0)
+        "/ik_target_0",
+        scale=0.2,
+        position=(0.21, -0.3, 0.26),
+        # wxyz=(0, 0, 1, 0),
+        wxyz=(-0.001 , 0.702 , 0.001 , 0.713),
     )
+
     ik_target_1 = server.scene.add_transform_controls(
-        "/ik_target_1", scale=0.2, position=(0.41, 0.3, 0.56), wxyz=(1, 0, 0, 0)
+        "/ik_target_1",
+        scale=0.2,
+        position=(0.21, 0.3, 0.26),
+        # wxyz=(0, 0, 1, 0),
+        wxyz=(-0.001 , 0.702 , 0.001 , 0.713),
+
+
     )
+
     timing_handle = server.gui.add_number("Elapsed (ms)", 0.001, disabled=True)
 
+    # ------------------------------------------------
+    # loop
+    # ------------------------------------------------
+
     while True:
+
         start_time = time.time()
 
-        # robot1 dual_xarm7
+        target_world = np.array([
+            ik_target_0.position,
+            ik_target_1.position
+        ])
+
+        target_rot = np.array([
+            ik_target_0.wxyz,
+            ik_target_1.wxyz
+        ])
+
+        # ---------------------------
+        # robot1 IK
+        # ---------------------------
+
+        # sol1 = pks.solve_ik_with_multiple_targets(
         sol1 = pks.solve_ik_with_multiple_targets(
             robot=robot1,
             target_link_names=target_link_names_1,
-            target_positions=np.array([ik_target_0.position, ik_target_1.position]),
-            target_wxyzs=np.array([ik_target_0.wxyz, ik_target_1.wxyz]),
+            target_positions=target_world - offset1,
+            target_wxyzs=target_rot,
         )
 
-        # robot2 openarm_bimanual
+        # ---------------------------
+        # robot2 IK
+        # ---------------------------
+
         sol2 = pks.solve_ik_with_multiple_targets(
             robot=robot2,
             target_link_names=target_link_names_2,
-            target_positions=np.array([ik_target_0.position, ik_target_1.position]),
-            target_wxyzs=np.array([ik_target_0.wxyz, ik_target_1.wxyz]),
+            target_positions=target_world - offset2,
+            target_wxyzs=target_rot,
         )
 
-        # robot3 g1_dual_arm
+        # ---------------------------
+        # robot3 IK
+        # ---------------------------
+
         sol3 = pks.solve_ik_with_multiple_targets(
             robot=robot3,
             target_link_names=target_link_names_3,
-            target_positions=np.array([ik_target_0.position, ik_target_1.position]),
-            target_wxyzs=np.array([ik_target_0.wxyz, ik_target_1.wxyz]),
+            target_positions=target_world - offset3,
+            target_wxyzs=target_rot,
         )
 
         elapsed_time = time.time() - start_time
         timing_handle.value = 0.99 * timing_handle.value + 0.01 * (elapsed_time * 1000)
 
-        # 更新三个机器人
+        # update visualization
+
         vis1.update_cfg(sol1)
         vis2.update_cfg(sol2)
         vis3.update_cfg(sol3)
